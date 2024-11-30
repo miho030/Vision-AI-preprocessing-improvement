482088ae1b33eaa3c2458641359a75fb9af73265
AOI <miho0_0@naver.com>
2024-12-01 08:41:40 +0900

    Add files via upload

--- reSize+CNN/PCA+CNN/PCA+CNN(InceptionV3)_32x32.py
+++ reSize+CNN/PCA+CNN/PCA+CNN(InceptionV3)_32x32.py
@@ -1,21 +1,65 @@
 # PCA + CNN (InceptionV3) 32x32
 
-import os, time
+import os, sys, time
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Input
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Flatten, Input, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-train_path = "../../_Dataset/cat_dog/training_set"
-test_path = "../../_Dataset/cat_dog/test_set"
+# 현재 프로젝트의 경로 활용
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+# 차원축소 데이터 저장 경로 지정
+npyResDir = "./pca_npyRes/pca_cnn_I_32/"
+os.makedirs(npyResDir, exist_ok=True)
+
+def get_dataset_paths():
+    def get_folder_size(path):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(path):
+            for file in filenames:
+                filepath = os.path.join(dirpath, file)
+                total_size += os.path.getsize(filepath) # 파일 크기를 누적
+        return total_size
+
+    if len(sys.argv) != 2:
+        print("\n" + "Usage: python script_name.py <choice> (1, 2, or 3)")
+        return None, None, None
+
+    choice = sys.argv[1]
+
+    if choice == "1":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/cat_dog/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/cat_dog/test_set"))
+    elif choice == "2":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/swimcat/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/swimcat/test_set"))
+    elif choice == "3":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/FER_2013/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/FER_2013/test_set"))
+    else:
+        print("Invalid choice. Please select 1, 2, or 3.")
+        return None, None, None
+
+    print("\n\n" + "=" * 60)
+    print("--- DataSet Selection ---")
+    print(f"Training Path: {train_path}")
+    print(f"Testing Path: {test_path}")
+    training_folder_size = get_folder_size(train_path) # 폴더 용량 계산
+    print(f"Training folder size: {training_folder_size / (1024**2):.2f} MB\n" + "="*60 + "\n")
+
+    return train_path, test_path, choice
+
+# 함수 호출 및 결과 출력
+train_path, test_path, choice = get_dataset_paths()
+if train_path == None:
+    exit(1)
 
 
 """PCA + 전처리 시간 측정 시작 """
@@ -32,7 +76,7 @@ training_set = datagen.flow_from_directory(
     target_size=(img_height, img_width),  # 원본 이미지 크기
     batch_size=32,           # 배치 크기 32로 설정
     shuffle=False,           # 순서 고정 (PCA에 중요)
-    class_mode='binary'
+    class_mode='categorical'
 )
 
 # 테스트 데이터 로드
@@ -41,7 +85,7 @@ test_set = datagen.flow_from_directory(
     target_size=(img_height, img_width),
     batch_size=32,           # 배치 크기 32로 설정
     shuffle=False,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
 # 전이학습 모델 불러오기
@@ -81,11 +125,21 @@ pca_end_time = time.time()
 cnn_start_time = time.time()
 
 # 모델 구성
+
+if choice == "1":
+    output = Dense(2, activation='softmax')  # 출력층
+elif choice == "2":
+    output = Dense(5, activation='softmax')  # 출력층
+elif choice == "3":
+    output = Dense(3, activation='softmax')  # 출력층
+
 model = Sequential([
     Input(shape=(n_components,)),
     Dense(128, activation='relu'),  # 은닉층
+    Dropout(0.5),
     Dense(64, activation='relu'),  # 추가 은닉층
-    Dense(2, activation='softmax')  # 출력층 (2 클래스 분류)
+    Dropout(0.5),
+    output
 ])
 
 # 모델 컴파일
@@ -113,14 +167,8 @@ history = model.fit(
     verbose=1
 )
 
-# 분류 모델 저장
-model_path = os.path.join("", "classification_inceptiont_32x32_dogcat.h5")
-model.save(model_path)
-print("Model saved.")
-
 """ CNN 세팅, 학습 시간 측정 종료 시점 """
 cnn_end_time = time.time()
-
 
 # 시간 산정
 pca_total_time = pca_end_time - pca_start_time
@@ -137,6 +185,40 @@ precision = precision_score(test_labels, predicted_classes)
 recall = recall_score(test_labels, predicted_classes)
 f1 = f1_score(test_labels, predicted_classes)
 
+
+# 학습된 가중치 모델 저장
+if choice == "1":
+    model_path = os.path.join("", "classification_inceptiont_32x32_dogcat.h5")
+    model.save(model_path)
+elif choice == "3":
+    model_path = os.path.join("", "classification_inceptiont_32x32_FEB2013.h5")
+    model.save(model_path)
+print("h5 Model saved.")
+
+
+# 차원 축소된 데이터 저장 및 용량 확인
+def measure_npy_file_sizes(directory):
+    print("\n\n" + "=" * 60)
+    np.save(npyResDir + "train_features_pca.npy", train_features_pca)
+    np.save(npyResDir + "test_features_pca.npy", test_features_pca)
+    np.save(npyResDir + "train_labels.npy", training_set.classes)
+    np.save(npyResDir + "test_labels.npy", test_set.classes)
+    print(f" -> PCA data saved at {npyResDir}..\n" + "="*60)
+    print(f"--- File sizes in {directory} ---\n")
+
+    total_size = 0
+    # 디렉토리의 모든 파일 확인
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):  # 파일인지 확인
+            file_size = os.path.getsize(file_path)  # 파일 크기 (바이트)
+            print(f"- {file_name}: {file_size / (1024 ** 2):.2f} MB")  # MB 단위로 출력
+            total_size += file_size
+
+    # 총 용량 출력
+    print(f"\nTotal size of all files: {total_size / (1024 ** 2):.2f} MB\n" + "="*60)
+# npyResDir 내 파일 용량 측정 함수 호출
+measure_npy_file_sizes(npyResDir)
 
 # 결과 출력
 print("\n\n" + "=" * 60)
--- reSize+CNN/PCA+CNN/PCA+CNN(MobileNet)_32x32.py
+++ reSize+CNN/PCA+CNN/PCA+CNN(MobileNet)_32x32.py
@@ -1,22 +1,67 @@
 # PCA + CNN (MobileNetV2) 32x32
 
-import os, time
+import os, sys, time
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Input
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Flatten, Input, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-train_path = "../../_Dataset/cat_dog/training_set"
-test_path = "../../_Dataset/cat_dog/test_set"
+# 현재 프로젝트의 경로 활용
+cwd = os.path.dirname(os.path.abspath(__file__))
 
+# 차원축소 데이터 저장 경로 지정
+npyResDir = "./pca_npyRes/pca_cnn_M_32/"
+os.makedirs(npyResDir, exist_ok=True)
+
+# 재귀함수 활용하여 결정된 데이터셋의 절대경로 및 용량을 구해 출력함.
+def get_dataset_paths():
+    def get_folder_size(path):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(path):
+            for file in filenames:
+                filepath = os.path.join(dirpath, file)
+                total_size += os.path.getsize(filepath) # 파일 크기를 누적
+        return total_size
+
+    if len(sys.argv) != 2:
+        print("\n" + "Usage: python script_name.py <choice> (1, 2, or 3)")
+        return None, None, None
+
+    choice = sys.argv[1]
+
+    if choice == "1":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/cat_dog/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/cat_dog/test_set"))
+    elif choice == "2":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/swimcat/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/swimcat/test_set"))
+    elif choice == "3":
+        train_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/FER_2013/training_set"))
+        test_path = os.path.abspath(os.path.join(cwd, "../../_Dataset/FER_2013/test_set"))
+    else:
+        print("Invalid choice. Please select 1, 2, or 3.")
+        return None, None, None
+
+    print("\n\n" + "=" * 60)
+    print("--- DataSet Selection ---")
+    print(f"Training Path: {train_path}")
+    print(f"Testing Path: {test_path}")
+    training_folder_size = get_folder_size(train_path) # 폴더 용량 계산
+    print(f"Training folder size: {training_folder_size / (1024**2):.2f} MB")
+    print("="*60 + "\n")
+
+    return train_path, test_path, choice
+
+# 함수 호출 및 결과 출력
+train_path, test_path, choice = get_dataset_paths()
+if train_path == None:
+    exit(1)
 
 """ PCA, 전처리 시간 측정 시작 """
 pca_start_time = time.time()
@@ -30,7 +75,7 @@ training_set = datagen.flow_from_directory(
     target_size=(img_height, img_width),
     batch_size=BATCH_SIZE,
     shuffle=False,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
 test_set = datagen.flow_from_directory(
@@ -38,7 +83,7 @@ test_set = datagen.flow_from_directory(
     target_size=(img_height, img_width),
     batch_size=BATCH_SIZE,
     shuffle=False,
-    class_mode='binary'
+    class_mode='categorical'
 )
 
 mobilenet = MobileNetV2(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
@@ -70,11 +115,20 @@ pca_end_time = time.time()
 """ CNN 시간 측정 시작 """
 cnn_start_time = time.time()
 
+if choice == "1":
+    output = Dense(2, activation='softmax')  # 출력층
+elif choice == "2":
+    output = Dense(5, activation='softmax')  # 출력층
+elif choice == "3":
+    output = Dense(3, activation='softmax')  # 출력층
+
 model = Sequential([
     Input(shape=(n_components,)),
-    Dense(128, activation='relu'), 
-    Dense(64, activation='relu'),
-    Dense(2, activation='softmax')
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(64, activation='relu'),  # 추가 은닉층
+    Dropout(0.5),
+    output
 ])
 
 model.compile(
@@ -98,14 +152,8 @@ history = model.fit(
     callbacks=[early_stopping],
     verbose=1
 )
-
-model_path = os.path.join("", "classification_mobilenet_32x32_dogcat.h5")
-model.save(model_path)
-print("Model saved.")
-
 """ CNN 세팅, 학습 시간 측정 종료 시점 """
 cnn_end_time = time.time()
-
 
 # 시간 산정
 pca_total_time = pca_end_time - pca_start_time
@@ -122,6 +170,40 @@ precision = precision_score(test_labels, predicted_classes)
 recall = recall_score(test_labels, predicted_classes)
 f1 = f1_score(test_labels, predicted_classes)
 
+
+# 학습된 가중치 모델 저장
+if choice == "1":
+    model_path = os.path.join(npyResDir, "classification_mobilenet_32x32_dogcat.h5")
+    model.save(model_path)
+elif choice == "3":
+    model_path = os.path.join(npyResDir, "classification_mobilenet_32x32_FEB2013.h5")
+    model.save(model_path)
+print("h5 Model saved.")
+
+# 차원 축소된 데이터 저장 및 용량 확인
+def measure_npy_file_sizes(directory):
+    print("\n\n" + "=" * 60)
+    np.save(npyResDir + "train_features_pca.npy", train_features_pca)
+    np.save(npyResDir + "test_features_pca.npy", test_features_pca)
+    np.save(npyResDir + "train_labels.npy", training_set.classes)
+    np.save(npyResDir + "test_labels.npy", test_set.classes)
+    print(f" -> PCA data saved at {npyResDir}..\n" + "="*60)
+    print(f"--- File sizes in {directory} ---\n")
+
+    total_size = 0
+    # 디렉토리의 모든 파일 확인
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):  # 파일인지 확인
+            file_size = os.path.getsize(file_path)  # 파일 크기 (바이트)
+            print(f" - {file_name}: {file_size / (1024 ** 2):.2f} MB")  # MB 단위로 출력
+            total_size += file_size
+
+    # 총 용량 출력
+    print(f"\nTotal size of all files: {total_size / (1024 ** 2):.2f} MB")
+    print("=" * 60)
+# npyResDir 내 파일 용량 측정 함수 호출
+measure_npy_file_sizes(npyResDir)
 
 # 결과 출력
 print("\n\n" + "=" * 60)
